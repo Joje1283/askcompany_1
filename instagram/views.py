6edb659bfbb02604b50bcc00c1ccf0a735d00f23
joje1283 <joje1283@gmail.com>
2020-10-18 13:37:29 +0900

    챕터3) 장고 Views 01 - 다양한 응답의 함수 기반 뷰 (1)

--- instagram/views.py
+++ instagram/views.py
@@ -1,14 +1,23 @@
+from django.views.generic import ListView
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Post
 
+post_list = ListView.as_view(model=Post)
 
-def post_list(request):
-    qs = Post.objects.all()
-    q = request.GET.get('q', '')
-    if q:
-        qs = qs.filter(message__icontains=q)  # 5분 5초
-        # instagram/templates/instagram/post_list.html
-    return render(request, 'instagram/post_list.html', {
-        'post_list': qs,
-        'q': q,
-    })
+# def post_list(request):
+#     qs = Post.objects.all()
+#     q = request.GET.get('q', '')
+#     if q:
+#         qs = qs.filter(message__icontains=q)  # 5분 5초
+#         # instagram/templates/instagram/post_list.html
+#     return render(request, 'instagram/post_list.html', {
+#         'post_list': qs,
+#         'q': q,
+#     })
+
+def post_detail(request, pk):
+    response = HttpResponse()
+    response.write("hello")
+    response.write("world")
+    return response
