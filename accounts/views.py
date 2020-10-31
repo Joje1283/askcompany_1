fcfe61a312bee8df10802eae631a6665d4f6efeb
joje1283 <joje1283@gmail.com>
2020-10-31 12:57:02 +0900

    챕터 6) 장고 기본 인증 구현하기 06. 회원가입하자마자 로그인하기

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, UpdateView, CreateView
-from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
 # @login_required
 # def profile(request):
 #     return render(request, 'accounts/profile.html')
@@ -40,12 +40,20 @@ def profile_edit(request):
     })
 
 
-signup = CreateView.as_view(
-    model=settings.AUTH_USER_MODEL,
-    form_class=UserCreationForm,
-    success_url=settings.LOGIN_URL,
-    template_name='accounts/signup_form.html'
-)
+class SignupView(CreateView):
+    model = settings.AUTH_USER_MODEL
+    form_class = UserCreationForm
+    success_url = settings.LOGIN_REDIRECT_URL
+    template_name = 'accounts/signup_form.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = self.object
+        auth_login(self.request, user)
+        return response
+
+
+signup = SignupView.as_view()
 
 
 def logout(request):
