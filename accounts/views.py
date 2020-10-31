fd86a24741fbe52be19d6cd3375a1ed042f694d6
joje1283 <joje1283@gmail.com>
2020-10-31 12:43:31 +0900

    챕터 6) 장고 기본 인증 구현하기 05. 회원가입

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, UpdateView
-
-
+from django.views.generic import TemplateView, UpdateView, CreateView
+from django.contrib.auth import get_user_model
 # @login_required
 # def profile(request):
 #     return render(request, 'accounts/profile.html')
@@ -35,11 +36,17 @@ def profile_edit(request):
     else:
         form = ProfileForm(instance=profile)
     return render(request, 'accounts/profile_form.html', {
-       'form': form
+        'form': form
     })
 
-def signup(request):
-    pass
+
+signup = CreateView.as_view(
+    model=settings.AUTH_USER_MODEL,
+    form_class=UserCreationForm,
+    success_url=settings.LOGIN_URL,
+    template_name='accounts/signup_form.html'
+)
+
 
 def logout(request):
     pass
